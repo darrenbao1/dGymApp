7e0d1410564b99780cff24fa1f572cfab96009ce
dbao <dbao@seraphlabs.io>
2023-12-09 12:33:43 +0800

    changing url to clone nextUrl

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -10,6 +10,7 @@ export async function GET(req: NextRequest) {
 	if (code) {
 		await supabase.auth.exchangeCodeForSession(code);
 	}
-
-	return NextResponse.redirect(new URL("/account", req.url));
+	const url = req.nextUrl.clone();
+	url.pathname = "/account";
+	return NextResponse.redirect(url);
 }
