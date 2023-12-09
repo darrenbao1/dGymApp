0e96cf4a470810f267abbf2285b310e203a2985d
dbao <dbao@seraphlabs.io>
2023-12-09 14:57:20 +0800

    removing createRouteHandlerClient

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
 	const cookieStore = cookies();
-	const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
+	//const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
 	// const { searchParams } = new URL(req.url);
 	// const code = searchParams.get("code");
 	// if (code) {
