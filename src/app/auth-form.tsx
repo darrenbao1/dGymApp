3c6e2a8228111d60d051b6c20c4b0600eafc48c1
dbao <dbao@seraphlabs.io>
2023-12-18 13:26:42 +0800

    fixing FOUC for Auth page

--- src/app/auth-form.tsx
+++ src/app/auth-form.tsx
@@ -1,8 +1,13 @@
 "use client";
-import { Auth } from "@supabase/auth-ui-react";
+//import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Database } from "../../types/supabase";
+import dynamic from "next/dynamic";
+const AuthWithNoSSR = dynamic(
+	() => import("@supabase/auth-ui-react").then((mod) => mod.Auth),
+	{ ssr: false }
+); // this is to solve FOUC for auth component.
 
 export default function AuthForm() {
 	const supabase = createClientComponentClient<Database>();
@@ -10,7 +15,7 @@ export default function AuthForm() {
 	return (
 		<div className=" bg-slate-500 w-screen h-screen flex justify-center items-center">
 			<div className="container p-6 shadow-xl bg-black rounded-xl">
-				<Auth
+				<AuthWithNoSSR
 					supabaseClient={supabase}
 					view="magic_link"
 					appearance={{
